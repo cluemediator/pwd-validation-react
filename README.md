1e9ac355051c461925b3613941dc728d2a4bf576
Clue Mediator <cluemediator@gmail.com>
2022-01-28 23:12:13 +0530

    Implement demo

---
+++ README.md
@@ -0,0 +1,30 @@
+# pwd-validation-react
+
+Password and Confirm Password validation in React
+
+## Documentation
+
+[https://www.cluemediator.com/password-and-confirm-password-validation-in-react](https://www.cluemediator.com/password-and-confirm-password-validation-in-react)
+
+## Quick Start
+
+Follow the below steps to run the project.
+
+1. Clone repository
+2. Run `npm i` command to install dependencies
+3. Execute `npm start` command to run the project
+
+## Output
+
+[![Toggle Switch](https://www.cluemediator.com/wp-content/uploads/2022/01/output-password-and-confirm-password-validation-in-react-clue-mediator.gif)](https://www.cluemediator.com/password-and-confirm-password-validation-in-react)
+
+**Follow us on [GitHub](https://github.com/cluemediator) for more update.**
+
+## Connect with us
+
+Website: [Clue Mediator](https://www.cluemediator.com)  
+Like us on [Facebook](https://www.facebook.com/thecluemediator)  
+Follow us on [Twitter](https://twitter.com/cluemediator)  
+Join us on [Telegram](https://t.me/cluemediator)  
+Subscribe us on [YouTube](https://www.youtube.com/ClueMediator)  
+Follow us on [Instagram](https://www.instagram.com/clue_mediator)
---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  const [input, setInput] = useState({
+    username: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const [error, setError] = useState({
+    username: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const onInputChange = e => {
+    const { name, value } = e.target;
+    setInput(prev => ({
+      ...prev,
+      [name]: value
+    }));
+    validateInput(e);
+  }
+
+  const validateInput = e => {
+    let { name, value } = e.target;
+    setError(prev => {
+      const stateObj = { ...prev, [name]: "" };
+
+      switch (name) {
+        case "username":
+          if (!value) {
+            stateObj[name] = "Please enter Username.";
+          }
+          break;
+
+        case "password":
+          if (!value) {
+            stateObj[name] = "Please enter Password.";
+          } else if (input.confirmPassword && value !== input.confirmPassword) {
+            stateObj["confirmPassword"] = "Password and Confirm Password does not match.";
+          } else {
+            stateObj["confirmPassword"] = input.confirmPassword ? "" : error.confirmPassword;
+          }
+          break;
+
+        case "confirmPassword":
+          if (!value) {
+            stateObj[name] = "Please enter Confirm Password.";
+          } else if (input.password && value !== input.password) {
+            stateObj[name] = "Password and Confirm Password does not match.";
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      return stateObj;
+    });
+  }
+
+  return (
+    <div className="app">
+      <h4>Password and Confirm Password validation in React - <a href="https://www.cluemediator.com" target="_blank">Clue Mediator</a></h4>
+      <form>
+
+        <input
+          type="text"
+          name="username"
+          placeholder='Enter Username'
+          value={input.username}
+          onChange={onInputChange}
+          onBlur={validateInput}></input>
+        {error.username && <span className='err'>{error.username}</span>}
+
+        <input
+          type="password"
+          name="password"
+          placeholder='Enter Password'
+          value={input.password}
+          onChange={onInputChange}
+          onBlur={validateInput}></input>
+        {error.password && <span className='err'>{error.password}</span>}
+
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder='Enter Confirm Password'
+          value={input.confirmPassword}
+          onChange={onInputChange}
+          onBlur={validateInput}></input>
+        {error.confirmPassword && <span className='err'>{error.confirmPassword}</span>}
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
